648dbae1e7e72aaa7a3f38f66677e5a3d23bca65
Mohammed Hamdy <mohammed.hamdy777@gmail.com>
2021-03-24 09:57:56 +0200

    Install react-router-dom and add basic search results page

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,27 @@
 import React from "react"
 
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+
 import AppBar from "./components/AppBar"
 import WrapperBox from "./components/WrapperBox"
 import BrandPage from "./pages/brand"
+import SearchResultsPage from "./pages/search-results"
 
 function App() {
   return (
-    <WrapperBox>
-      <AppBar/>
-      <BrandPage/>
-    </WrapperBox>
+    <Router>
+      <WrapperBox>
+        <AppBar/>
+        <Switch>
+          <Route path="/search">
+            <SearchResultsPage/>
+          </Route>
+          <Route path="/">
+            <BrandPage/>
+          </Route>
+      </Switch>
+      </WrapperBox>
+    </Router>
   )
 }
 
---
+++ src/pages/search-results/SearchResults.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+import Typography from "@material-ui/core/Typography"
+
+
+export default function SearchResults() {
+    return (
+        <Typography variant="h4">Search results for the query "Tommy"</Typography>
+    )
+}

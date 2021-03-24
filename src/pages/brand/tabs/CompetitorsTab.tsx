b3f80184b591a05e9e6a1a297a120fe95657feb5
Mohammed Hamdy <mohammed.hamdy777@gmail.com>
2021-03-24 07:41:43 +0200

    Add horizontal line chart to competitors tab

--- src/pages/brand/tabs/CompetitorsTab.tsx
+++ src/pages/brand/tabs/CompetitorsTab.tsx
@@ -5,6 +5,7 @@ import Button from "@material-ui/core/Button"
 import Chip from "@material-ui/core/Chip"
 import Grid from "@material-ui/core/Grid"
 import IconButton from "@material-ui/core/IconButton"
+import Switch from "@material-ui/core/Switch"
 import Table from "@material-ui/core/Table"
 import TableBody from "@material-ui/core/TableBody"
 import TableCell from "@material-ui/core/TableCell"
@@ -61,6 +62,15 @@ const useStyles = makeStyles((theme: Theme) => {
             marginTop: theme.spacing(1),
             display: "flex",
             justifyContent: "center"
+        },
+        buttonGroupContainer: {
+            "& > button": {
+                marginRight: theme.spacing(2)
+            }
+        },
+        svgScaleText: {
+            fill: "#6E788A",
+            fontWeight: "bold"
         }
     })
 })
@@ -320,58 +330,124 @@ function BrandTable() {
     )
 }
 
-export default function CompetitorsTab() {
-    const classes = useStyles()
+function BarChart() {
     const brandNames = ["Apple", "Google", "Coca-Cola", "Microsoft", "Toyota", "IBM", "Samsung", "Amazon", "Mercedes-Benz", "GE", "BMW", "McDonald's", "Disney", "Intel", "Facebook", "Cisco", "Oracle", "Nike", "Louis Vuitton", "H&M", "Honda", "SAP", "Pepsi", "Gillette", "American", "Express", "IKEA", "Zara", "Pampers", "UPS", "Budweiser", "J.P.", "Morgan", "eBay", "Ford", "Herm\xc3\xa8s", "Hyundai", "Nescafe", "Accenture", "Audi", "Kellogg's", "Volkswagen", "Philips", "Canon", "Nissan", "Hewlett", "Packard", "Enterprise", "L'Or\xc3\xa9al", "AXA", "HSBC", "HP", "Citi", "Porsche", "Allianz", "Siemens", "Gucci", "Goldman", "Sachs", "Danone", "Nestl\xc3\xa9", "Colgate", "Sony", "3M", "adidas", "Visa", "Cartier", "Adobe", "Starbucks", "Morgan", "Stanley", "Thomson", "Reuters", "Lego", "Panasonic", "Kia", "Santander", "Discovery", "Huawei", "Johnson & Johnson", "Tiffany & Co.", "KFC", "MasterCard", "DHL", "Land", "Rover", "FedEx", "Harley-Davidson", "Prada", "Caterpillar", "Burberry", "Xerox", "Jack", "Daniel's", "Sprite", "Heineken", "Mini", "Dior", "PayPal", "John", "Deere", "Shell", "Corona", "MTV", "Johnnie", "Walker", "Smirnoff", "Mo\xc3\xabt", "&", "Chandon", "Ralph", "Lauren", "Lenovo", "Tesla"]
     const barData = new Array(50).fill(0).map((_, index) => ({brandName: brandNames[index], scalingScore: Math.round(Math.random() * 10 * 100) / 100, index}))
     return (
+        <Grid item xs={12} style={{ height: "400px"}}>
+            <ResponsiveBar
+                data={barData}
+                keys={[ "scalingScore" ]}
+                indexBy="index"
+                margin={{ top: 16, right: 16, bottom: 32, left: 16 }}
+                padding={0.3}
+                valueScale={{ type: "linear" }}
+                indexScale={{ type: "band", round: true }}
+                colors="#DEDFE2"
+                borderColor={{ from: "color", modifiers: [ [ "darker", 1.6 ] ] }}
+                axisTop={null}
+                axisRight={null}
+                axisBottom={{
+                    tickSize: 5,
+                    tickPadding: 5,
+                    tickRotation: 0,
+                }}
+                label={(rowData) => `${(rowData.data as unknown as BrandBarChartRow).brandName}: ${rowData.value}`}
+                labelTextColor={{ from: "color", modifiers: [ [ "darker", 1.6 ] ] }}
+                tooltip={({ data, value, color }) => (
+                    <strong style={{ color }}>
+                        {data.brandName}: {value}
+                    </strong>
+                )}
+                theme={{
+                    tooltip: {
+                        container: {
+                            background: "#333",
+                        },
+                    },
+                }}
+                animate={true}
+                motionStiffness={90}
+                motionDamping={15}
+            />
+        </Grid>
+    )
+}
+
+function HorizontalGradientChart() {
+    const classes = useStyles()
+    interface ChartData {
+        name: string
+        percent: number
+        avatar: string
+    }
+    const brandNames = ["Apple", "Google", "Coca-Cola", "Microsoft", "Toyota", "IBM", "Samsung", "Amazon", "Mercedes-Benz", "GE", "BMW"]
+    const data: ChartData[] = new Array(10).fill(0).map((value, index) => ({
+        name: brandNames[index],
+        avatar: "/images/brands/tommyhilfiger/avatar.png",
+        percent: Math.random() * 100
+    }))
+    return (
+        <Grid item xs={12} style={{ height: "400px" }}>
+            <svg width="100%" height="400" xmlns="http://www.w3.org/2000/svg">
+                <defs>
+                    <linearGradient
+                        id="redToGreenGradient"
+                        gradientUnits="userSpaceOnUse"
+                        x1="0" x2="100%" y1="0" y2="0">
+                        <stop offset="0%" stopColor="#C23D4B"/>
+                        <stop offset="50%" stopColor="#FCD669"/>
+                        <stop offset="100%" stopColor="#169570"/>
+                    </linearGradient>
+                </defs>
+                <line
+                    x1="0"
+                    x2="100%"
+                    y1="200"
+                    y2="200"
+                    stroke="url(#redToGreenGradient)"
+                    style={{ strokeWidth: 15 }}
+                />
+                <text x="0" y="225" className={classes.svgScaleText}>0</text>
+                <text x="97.5%" y="225" className={classes.svgScaleText}>100</text>
+                {/* draw vertical black lines perpendicular to scale */}
+                {data.map((dataItem, index) => (
+                    <>
+                        <line key={index} x1={`${dataItem.percent}%`} x2={`${dataItem.percent}%`} y1="208" y2="180" strokeWidth="1" stroke="#8792A2"/>
+                        <image href={dataItem.avatar} width="32" height="32" x={`${dataItem.percent - 1.2}%`} y="150"/>
+                    </>
+                ))}
+            </svg>
+        </Grid>
+    )
+}
+
+function ButtonGroup() {
+    const [selectedButtonIndex, setSelectedButtonIndex] = React.useState(0)
+    const buttonTexts = ["Scaling Score", "Revenue Score", "Socials", "Reviews", "Reddit"]
+    return (
+        <>
+            {buttonTexts.map((buttonText, index) => <Button key={index} color="primary" size="small" variant={selectedButtonIndex == index ? "contained" : "outlined"} onClick={() => setSelectedButtonIndex(index)}>{buttonText}</Button>)}
+        </>
+    )
+}
+
+export default function CompetitorsTab() {
+    const classes = useStyles()
+    const [chartFlipped, setChartFlipped] = React.useState(false)
+    return (
         <Grid container>
             <Grid container item xs={12}>
-                <Grid item xs={3}>
-                    <Typography variant="h5">Similar brands</Typography>
+                <Grid item xs={6} className={classes.buttonGroupContainer}>
+                    {chartFlipped ? <ButtonGroup/> : <Typography variant="h5">Similar brands</Typography>}
                 </Grid>
-                <Grid container item xs={9} justify="flex-end">
+                <Grid container item xs={6} justify="flex-end">
+                    <Switch checked={chartFlipped} onChange={() => setChartFlipped(!chartFlipped)}/>
                     <Button startIcon={<DateRangeIcon/>} endIcon={<KeyboardArrowDownIcon/>} variant="outlined" size="small">Last 6 months</Button>
                     <Button endIcon={<KeyboardArrowDownIcon/>} variant="outlined" size="small" style={{marginLeft: 8}}>10 competitors</Button>
                 </Grid>
             </Grid>
-            <Grid item xs={12} style={{ height: "400px"}}>
-                <ResponsiveBar
-                    data={barData}
-                    keys={[ "scalingScore" ]}
-                    indexBy="index"
-                    margin={{ top: 16, right: 16, bottom: 32, left: 16 }}
-                    padding={0.3}
-                    valueScale={{ type: "linear" }}
-                    indexScale={{ type: "band", round: true }}
-                    colors="#DEDFE2"
-                    borderColor={{ from: "color", modifiers: [ [ "darker", 1.6 ] ] }}
-                    axisTop={null}
-                    axisRight={null}
-                    axisBottom={{
-                        tickSize: 5,
-                        tickPadding: 5,
-                        tickRotation: 0,
-                    }}
-                    label={(rowData) => `${(rowData.data as unknown as BrandBarChartRow).brandName}: ${rowData.value}`}
-                    labelTextColor={{ from: "color", modifiers: [ [ "darker", 1.6 ] ] }}
-                    tooltip={({ data, value, color }) => (
-                        <strong style={{ color }}>
-                            {data.brandName}: {value}
-                        </strong>
-                    )}
-                    theme={{
-                        tooltip: {
-                            container: {
-                                background: "#333",
-                            },
-                        },
-                    }}
-                    animate={true}
-                    motionStiffness={90}
-                    motionDamping={15}
-                />
-            </Grid>
+            {chartFlipped ? <HorizontalGradientChart/> : <BarChart/>}
             <Grid item xs={12}>
                 <BrandTable/>
             </Grid>
